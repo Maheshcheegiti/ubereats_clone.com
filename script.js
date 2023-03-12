94d9e678c9c65125533e4e4ffbb8a9f019749358
Cheegiti Mahesh <ramahesh024@gmail.com>
2023-03-12 19:30:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+window.addEventListener('scroll', function() {
+    const myDiv = document.querySelector('header');
+    const myDiv2 = document.getElementById('mbar');
+    var fl = 0;
+    if (window.scrollY > 0) {
+        myDiv.classList.add('scrolled');
+    } else {
+        myDiv.classList.remove('scrolled');
+    }
+    if(window.scrollY > 400){
+        myDiv2.classList.add('show');
+        myDiv2.classList.remove('hide');
+        f=1;
+    }else if(f==1){
+        myDiv2.classList.add('hide');
+        f=0;
+    }
+});
+const optionMenu = document.querySelector(".select-menu"),
+selectBtn = optionMenu.querySelector(".select-btn");
+selectBtn.addEventListener("click",()=> optionMenu.classList.toggle("active"));
+const stopScrollingCheckbox = document.getElementById('btn');
+const content = document.getElementById('body');
+stopScrollingCheckbox.addEventListener('change', function() {
+  if (this.checked) {
+    content.style.overflow = 'hidden';
+  } else {
+    content.style.overflow = 'auto';
+  }
+});
